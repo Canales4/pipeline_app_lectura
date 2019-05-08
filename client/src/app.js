a5357cc95a0a97cd8430291b5f3f9abe63dbb467
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-08 15:54:08 +0200

    modificando el pipeline

---
+++ client/src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const bodyParser  = require("body-parser");
+///const methodOverride = require("method-override");
+
+
+var app = express();
+var resultado = 0;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || "3000";
+
+app.use(express.static(path.join(__dirname, '../dist'))); 
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.use(router);
+
+app.listen(port, function() {
+  console.log("Node server running on http://"+host+":"+port);
+
+});
