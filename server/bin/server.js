ef4c3c6cdd3307147dd2e80bfec1873f639a0b3c
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-15 13:26:42 +0200

    modificando server.js

--- server/bin/server.js
+++ server/bin/server.js
@@ -2,7 +2,7 @@ var app = require('../app');
 var os = require("os");
 var colors = require("colors");
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get("port"), (req, res) => {
     console.log("\nServer running...".green);
