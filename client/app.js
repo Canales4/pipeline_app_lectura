25e2be4d9e2fb7f727449eed1daf2b1469c897c8
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-15 13:01:12 +0200

    modificando el pipeline

--- client/app.js
+++ client/app.js
@@ -14,11 +14,11 @@ app.use(bodyParser.json());
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || "3000";
 
-app.use(express.static(path.join(__dirname, '../client/dist/client')));
+app.use(express.static(path.join(__dirname, '../client/src')));
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/dist/client/index.html'));
+    res.sendFile(path.join(__dirname, '../client/src/index.html'));
 });
 
 app.use(router);
