f849145521e7bff07ec266eab01c28be00d3d83f
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-16 12:27:24 +0200

    modificando el pipeline

--- server/bin/server.js
+++ server/bin/server.js
@@ -3,7 +3,7 @@ var os = require("os");
 var colors = require("colors");
 
 app.set('port', process.env.PORT || 3000);
-app.setInterval(1500);
+
 app.listen(app.get("port"), (req, res) => {
     console.log("\nServer running...".green);
     console.log("Server on : ".green, os.type().red)
