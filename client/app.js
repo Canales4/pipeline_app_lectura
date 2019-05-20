033e3516211bcc6b790c53a0bcc3d379c2932109
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-20 15:48:45 +0200

    modificando el Jenkinsfile y app.js

--- client/app.js
+++ client/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 
 const host = process.env.HOST || "localhost";
-const port = process.env.PORT || "3000";
+const port = process.env.PORT || "3001";
 
 app.use(express.static(path.join(__dirname, '../app/dist/client')));
 var router = express.Router();
