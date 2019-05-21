d31c39a102de5454e1801de33ba4ebf0e4b63c40
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-21 13:00:37 +0200

    modificando package.json

--- client/app.js
+++ client/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const bodyParser  = require("body-parser");
+const httpProxy = require("http-proxy");
 ///const methodOverride = require("method-override");
 
 var app = express();
@@ -9,6 +10,9 @@ var resultado = 0;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get("/api/*", function(req, res){
+
+});
 
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || "3000";
@@ -16,8 +20,11 @@ const port = process.env.PORT || "3000";
 app.use(express.static(path.join(__dirname, '../app/dist/client')));
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../app/dist/client/index.html'));
+var apiProxy = httpProxy.createProxyServer();
+
+router.get('/home', function(req, res) {
+    //res.sendFile(path.join(__dirname, '../app/dist/client/index.html'));
+    apiProxy.web(req, res, { target: 'https://applectura-server.eu-gb.mybluemix.net' });
 });
 
 app.use(router);
