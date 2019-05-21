12e80d95636b9c3899709f66dfd4e8477ed1735a
Juan Carlos <canaleslinaresjuancarlos@gmail.com>
2019-05-21 13:24:24 +0200

    control-z

--- client/app.js
+++ client/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const bodyParser  = require("body-parser");
-const httpProxy = require("http-proxy");
 ///const methodOverride = require("method-override");
 
 var app = express();
@@ -10,21 +9,20 @@ var resultado = 0;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || "3000";
 
 app.use(express.static(path.join(__dirname, '../app/dist/client')));
 var router = express.Router();
 
-var apiProxy = httpProxy.createProxyServer();
-
-router.get('/home', function(req, res) {
-    //res.sendFile(path.join(__dirname, '../app/dist/client/index.html'));
-    apiProxy.web(req, res, { target: 'https://applectura-server.eu-gb.mybluemix.net' });
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../app/dist/client/index.html'));
 });
 
 app.use(router);
 
 app.listen(port, function() {
   console.log("Node server running on http://"+host+":"+port);
+
 });
